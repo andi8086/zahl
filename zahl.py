db22435e8ef22c57eeeff038b86c1906ea5bfa2e
Andreas Reichel <webmaster@6th-dimension.com>
2019-02-23 12:37:31 +0100

    Simplify algorithm

--- zahl.py
+++ zahl.py
@@ -30,51 +30,39 @@ def convert_group(number):
 			["nongenti"]]
 
 	res = ""
-
+	
 	h = int(number / 100)
 	z = int((number % 100) / 10)
 	e = number % 10
 
-	es = ""
-	zs = ""
-	hs = ""
-
-	res = ""
-
-	if h == 0 and z == 0:
-		res =  ones_alone[e-1]
+	ls = ""
+	lead = [] 
 	
-	elif z != 0:
-		if len(tens[z-1]) == 1 or e == 0:
-			res = tens[z-1][0]
-		elif len(ones_combine[e-1]) == 1:
-			res = ones_combine[e-1][0] + "-" + tens[z-1][0]
+	if h == 0 and z == 0:
+		return  ones_alone[e-1]
+	
+	if h != 0:
+		lead = hundreds[h-1]
+
+	if z != 0:
+		lead = tens[z-1]
+
+	if len(lead) == 1 or e == 0:
+		res = lead[0]
+	elif len(ones_combine[e-1]) == 1:
+		res = ones_combine[e-1][0] + "-" + lead[0]
+	else:
+		# several possibilities for z sylable, find matching e syl
+		ls = [[s[1:],t] for s in lead for t in ones_combine[e-1] if s[0] == t[-1]]
+
+		# no valid combination? take the first sylabels
+		if len(ls) == 0:
+			res = ones_combine[e-1][0] + "-" + lead[0]
 		else:
-			# several possibilities for z sylable, find matching e syl
-			zs = [[s[1:],t] for s in tens[z-1] for t in ones_combine[e-1] if s[0] == t[-1]]
+			res = ls[0][1] + "-" + ls[0][0]
 
-			# no valid combination? take the first sylabels
-			if len(zs) == 0:
-				res = ones_combine[e-1][0] + "-" + tens[z-1][0]
-			else:
-				res = zs[0][1] + "-" + zs[0][0]
-			#res = zs[1]+zs[0]
-
-
-		if h != 0:
+	if z != 0 and h != 0:
 			res = res + "-" + hundreds[h-1][0]
-
-	elif h != 0:
-		if len(hundreds[h-1]) == 1 or e == 0:
-			res = hundreds[h-1][0]
-		elif len(ones_combine[e-1]) == 1:
-			res = ones_combine[e-1][0] + "-" + hundreds[h-1][0]
-		else:
-			hs = [[s[1:],t] for s in hundreds[h-1] for t in ones_combine[e-1] if s[0] == t[-1]]
-			if len(hs) == 0:
-				res = ones_combine[e-1][0] + "-" + hundreds[h-1][0]
-			else:
-				res = hs[0][1] + "-" + hs[0][0]
 
 	return res
 	
@@ -116,24 +104,22 @@ while len(m) > 0:
 		m = ""
 	r = ""
 	if p == 0:
-		r = "ni" + "-"
+		r = "ni"
 	else:
 		r = convert_group(p)[:-1] + "i"
 	numstr = r + "-" + "lli" + "-" + numstr
-
-
 
 s = (numstr[0].upper())
 numstr = s + numstr[1:]
 
 if n == False:
 	numstr = numstr + "on"
-	if o > 0:
-		numstr = ["Zehn ", "Hundert "][o-1] + numstr + "en"
 else:
 	numstr = numstr + "arde"
-	if o > 0:
-		numstr = ["Zehn ", "Hundert "][o-1] + numstr + "n"
 
+if o > 0:
+	if n == False:
+		numstr = numstr + "e"
+	numstr = ["Zehn ", "Hundert "][o-1] + numstr + "n"
 
 print("    " + numstr)
